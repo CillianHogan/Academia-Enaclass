85b067a3cba6f56d133e351061966adaad2bd5d2
Cillian Hogan <HoganLobato@gmail.com>
2026-04-27 12:33:28 +0200

    Estructura de header terminado con clases para luego meter en css

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia Enaclass | Inicio</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body class="cuerpo-web">
+    <header class ="encabezado-inicio">
+        <div class="contenedor">
+            <a href="index.php">
+                <h1 class="logo-academia">Enaclass</h1>
+            </a>
+            <nav class="menu-derecha">
+                <button id="boton-oscuro" class="boton-modo-oscuro">Modo oscuro</button>
+                <a href="index.php">Inicio</a>
+                <a href="servicios.php">Servicios</a>
+                <a href="#contacto" class="boton-contacto">Contacto</a>
+            </nav>
+        </div>
+    </header>
+</body>
+</html>
