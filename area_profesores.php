f4553cbb4540c5bd6e584b7310519c89bb8fcb42
Cillian Hogan <HoganLobato@gmail.com>
2026-05-03 02:21:07 +0200

    Reestructuración del area de profesores, el anterior se veia muy cargado, se ha modificado para que tenga un menu como el area de estudiantes y pueda seleccionar la acción que quiere llevar a cabo, la estética es la misma que el área de estudiantes, solo se ha añadido un color para la generación de los tests

--- area_profesores.php
+++ area_profesores.php
@@ -21,161 +21,58 @@
         </div>
     </header>
 
-    <main class="zona-administracion">
-
-        <div class="cabecera-gestion">
-            <h2>Panel de Gestión Docente</h2>
-            <p>Control total sobre el alumnado, contenido académico y administración financiera.</p>
-            <div class="rayita-naranja"></div>
-        </div>
-
-        <div class="rejilla-admin">
-            <section class="caja-herramienta borde-violeta ancho-completo">
-                <h3>Dar de Alta Nuevo Alumno</h3>
-                    <form action="#" method="POST" class="formulario-admin">
-                        <div class="fila-input">
-                            <label>Nombre completo</label>
-                            <input type="text" name="nombre" required placeholder="Nombre del alumno aquí" class="input-diseño">
-                        </div>
-
-                        <div class="fila-input">
-                            <label>Correo electrónico</label>
-                            <input type="email" name="correo" required placeholder="Ej: alumno@enaclass.com" class="input-diseño">
-                        </div>
-
-                        <div class="fila-input">
-                            <label>Contraseña temporal</label>
-                            <input type="password" name="clave" required placeholder="Enaclass 2026" class="input-diseño">
-                        </div>
-                        <button type="submit" class="boton-accion-lila">REGISTRAR ALUMNO</button>
-                    </form>
-            </section>
-
-            <section class="caja-herramienta borde-violeta ancho-completo">
-                <h3>Dar de Alta a Padre</h3>
-                    <form action="#" method="POST" class="formulario-admin">
-                        <div class="fila-input">
-                            <label>Nombre del Tutor</label>
-                            <input type="text" name="nombre" required class="input-diseño">
-                        </div>
-                        <div class="fila-input">
-                            <label>Vincular con Alumno</label>
-                            <select name="id_hijo" class="input-diseño">
-                                <option value="">Selecciona un alumno...</option>
-                            </select>
-                        </div>
-                        <button type="submit" class="boton-accion-lila">VINCULAR TUTOR</button>
-                    </form>
-            </section>
-
-            <section class="caja-herramienta borde-azul ancho-completo">
-                <h3>Alumnos Registrados</h3>
-                <div class="tabla-scroll">
-                    <table class="tabla-gestion">
-                        <thead>
-                            <tr>
-                                <th>Nombre</th>
-                                <th>Email</th>
-                                <th>Accion</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>Nombre de ejemplo</td>
-                                <td>ejemplo@enaclass.com</td>
-                                <td><a href="#" class="enlace-baja">Baja</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-
-            <section class="caja-herramienta borde-violeta">
-                <h3>Pasar Lista</h3>
-                <form action="#" method="POST">
-                    <div class="fila-input">
-                        <label>Fecha:</label>
-                        <input type="date" nombre="fecha" class="input-diseño">
-                    </div>
-                    <table class="tabla-gestion">
-                        <thead><tr><th>Alumno</th><th>Estado</th></tr></thead>
-                        <tbody>
-                            <tr>
-                                <td>Alumno de prueba</td>
-                                <td><select class="input-diseño"><option>Presente</option><option>Falta</option></select></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button type="submit" class="boton-accion-lila">Guardar Asistencia</button>
-                </form>
-            </section>
-
-            <section class="caja-herramienta borde azul">
-                <h3>Subir Material Didáctico</h3>
-                <form action="#" method="POST" enctype="multipart/form-data">
-                    <div class="fila-input">
-                        <label>Título del archivo</label>
-                        <input type="text" class="input-diseño" placeholder="Ej: Tema 1 - Verbo To Be">
-                    </div>
-
-                    <div class="fila-input">
-                        <label>Elige a que alumnos enviar el material:</label>
-                            <div class="selector-multi-almunos">
-                                <label class="opcion-alumno">
-                                    <input type="checkbox" name="alumnos_destino[]" value="1"> Carlos Sánchez
-                                </label>
-                                <label class="opcion-alumno">
-                                    <input type="checkbox" name="alumnos_destino[]" value="2"> David Sola
-                                </label>
-                                <label class="opcion-alumno">
-                                    <input type="checkbox" name="alumnos_destino[]" value="3"> David Jiménez
-                                </label>
-                                <label class="opcion-alumno">
-                                    <input type="checkbox" name="alumnos_destino[]" value="4"> Carmelo Navarro
-                                </label>
-                            </div>
-                        <small class="ayuda-formulario">Marca los alumnos que recibirán el archivo.</small>
-                    </div>
-
-                    <div class="fila-input">
-                        <label>Seleccionar archivo</label>
-                        <input type="file" class="input-diseño">
-                    </div>
-                    <button type="submit" class="boton-accion-lila">Publicar Material</button>
-                </form>
-            </section>
-
-            <section class="caja-herramienta borde-azul ancho-completo">
-                <h3>Foro: Responder Consultas</h3>
-                <div class="tabla-scroll">
-                    <table class="tabla-gestion">
-                        <thead>
-                            <tr>
-                                <th>Alumno y Pregunta</th>
-                                <th>Tu respuesta</th>
-                                <th>Acción</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <strong>Paco Díaz</strong>
-                                    <p>¿Hay clase el lunes?</p>
-                                </td>
-                                <td>
-                                    <input type="text" placeholder="Escribe tu respuesta..." class="input-diseño">
-                                </td>
-                                <td>
-                                    <button type="submit" class="boton-responder">Enviar</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </section>
-
-        </div>
+    <main class="contenedor-panel">
         
+        <section class="bienvenida-usuario">
+            <h2>Bienvenido, nombre del profesor con php</h2>
+            <p>¿Qué gestión necesitas realizar hoy?</p>
+            <div class="rayita-naranja"></div>
+        </section>
+
+        <div class="rejilla-opciones">
+
+        <a href="#" class="caja-opcion color-asignaturas">
+            <div class="contenido-caja">
+                <h3>Gestión de Alumnos</h3>
+                <p>Dar de alta a alumnos, tutores y ver el listado actual.</p>
+            </div>
+        </a>
+
+        <a href="#" class="caja-opcion color-asistencia">
+            <div class="contenido-caja">
+                <h3>Pasar Lista</h3>
+                <p>Registrar la asistencia diaria de tus grupos.</p>
+            </div>
+        </a>
+
+        <a href="#" class="caja-opcion color-notas">
+            <div class="contenido-caja">
+                <h3>Material Didáctico</h3>
+                <p>Sube apuntes y recursos para tus alumnos.</p>
+            </div>
+        </a>
+
+        <a href="#" class="caja-opcion color-tests">
+            <div class="contenido-caja">
+                <h3>Generador de Tests</h3>
+                <p>Crea exámenes evaluativos para tus alumnos.</p>
+            </div>
+        </a>
+
+        <a href="#" class="caja-opcion color-facturas">
+            <div class="contenido-caja">
+                <h3>Facturación</h3>
+                <p>Genera recibos y facturas para que los alumnos las descarguen.</p>
+            </div>
+        </a>
+
+        <a href="#" class="caja-opcion color-foro">
+            <div class="contenido-caja">
+                <h3>Foro de Dudas</h3>
+                <p>Revisa y responde las consultas de tus estudiantes.</p>
+            </div>
+        </a>
+        </div>
     </main>
 
     <footer class="pie-pagina">
---
+++ area_profesores1.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Academia Enaclass | Inicio</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body class="cuerpo-web">
+    <header class ="encabezado-inicio">
+        <div class="contenedor">
+            <a href="index.php">
+                <h1 class="logo-academia">Enaclass</h1>
+            </a>
+            <nav class="menu-derecha">
+                <button id="boton-oscuro" class="boton-modo-oscuro">Modo oscuro</button>
+                <a href="index.php">Inicio</a>
+                <a href="servicios.php">Servicios</a>
+                <a href="#contacto" class="boton-contacto">Contacto</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="zona-administracion">
+
+        <div class="cabecera-gestion">
+            <h2>Panel de Gestión Docente</h2>
+            <p>Control total sobre el alumnado, contenido académico y administración financiera.</p>
+            <div class="rayita-naranja"></div>
+        </div>
+
+        <div class="rejilla-admin">
+            <section class="caja-herramienta borde-violeta ancho-completo">
+                <h3>Dar de Alta Nuevo Alumno</h3>
+                    <form action="#" method="POST" class="formulario-admin">
+                        <div class="fila-input">
+                            <label>Nombre completo</label>
+                            <input type="text" name="nombre" required placeholder="Nombre del alumno aquí" class="input-diseño">
+                        </div>
+
+                        <div class="fila-input">
+                            <label>Correo electrónico</label>
+                            <input type="email" name="correo" required placeholder="Ej: alumno@enaclass.com" class="input-diseño">
+                        </div>
+
+                        <div class="fila-input">
+                            <label>Contraseña temporal</label>
+                            <input type="password" name="clave" required placeholder="Enaclass 2026" class="input-diseño">
+                        </div>
+                        <button type="submit" class="boton-accion-lila">REGISTRAR ALUMNO</button>
+                    </form>
+            </section>
+
+            <section class="caja-herramienta borde-violeta ancho-completo">
+                <h3>Dar de Alta a Padre</h3>
+                    <form action="#" method="POST" class="formulario-admin">
+                        <div class="fila-input">
+                            <label>Nombre del Tutor</label>
+                            <input type="text" name="nombre" required class="input-diseño">
+                        </div>
+                        <div class="fila-input">
+                            <label>Vincular con Alumno</label>
+                            <select name="id_hijo" class="input-diseño">
+                                <option value="">Selecciona un alumno...</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="boton-accion-lila">VINCULAR TUTOR</button>
+                    </form>
+            </section>
+
+            <section class="caja-herramienta borde-azul ancho-completo">
+                <h3>Alumnos Registrados</h3>
+                <div class="tabla-scroll">
+                    <table class="tabla-gestion">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Acción</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Nombre de ejemplo</td>
+                                <td>ejemplo@enaclass.com</td>
+                                <td><a href="#" class="enlace-baja">Baja</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+
+            <section class="caja-herramienta borde-violeta">
+                <h3>Pasar Lista</h3>
+                <form action="#" method="POST">
+                    <div class="fila-input">
+                        <label>Fecha:</label>
+                        <input type="date" nombre="fecha" class="input-diseño">
+                    </div>
+                    <table class="tabla-gestion">
+                        <thead><tr><th>Alumno</th><th>Estado</th></tr></thead>
+                        <tbody>
+                            <tr>
+                                <td>Alumno de prueba</td>
+                                <td><select class="input-diseño"><option>Presente</option><option>Falta</option></select></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button type="submit" class="boton-accion-lila">Guardar Asistencia</button>
+                </form>
+            </section>
+
+            <section class="caja-herramienta borde-azul">
+                <h3>Subir Material Didáctico</h3>
+                <form action="#" method="POST" enctype="multipart/form-data">
+                    <div class="fila-input">
+                        <label>Título del archivo</label>
+                        <input type="text" class="input-diseño" placeholder="Ej: Tema 1 - Verbo To Be">
+                    </div>
+
+                    <div class="fila-input">
+                        <label>Elige a qué alumnos enviar el material:</label>
+                            <div class="selector-multi-alumnos">
+                                <label class="opcion-alumno">
+                                    <input type="checkbox" name="alumnos_destino[]" value="1"> Carlos Sánchez
+                                </label>
+                                <label class="opcion-alumno">
+                                    <input type="checkbox" name="alumnos_destino[]" value="2"> David Sola
+                                </label>
+                                <label class="opcion-alumno">
+                                    <input type="checkbox" name="alumnos_destino[]" value="3"> David Jiménez
+                                </label>
+                                <label class="opcion-alumno">
+                                    <input type="checkbox" name="alumnos_destino[]" value="4"> Carmelo Navarro
+                                </label>
+                            </div>
+                        <small class="ayuda-formulario">Marca los alumnos que recibirán el archivo.</small>
+                    </div>
+
+                    <div class="fila-input">
+                        <label>Seleccionar archivo</label>
+                        <input type="file" class="input-diseño">
+                    </div>
+                    <button type="submit" class="boton-accion-lila">Publicar Material</button>
+                </form>
+            </section>
+
+            <section class="caja-herramienta borde-naranja ancho-completo">
+                <h3>Crear Nuevo Test Evaluativo</h3>
+                <form action="guardar_test.php" method="POST" class="formulario-admin">
+                    
+                    <div class="fila-ajustes-examen">
+                        <div class="fila-input">
+                            <label>Título del Test</label>
+                            <input type="text" name="titulo_test" placeholder="Ej: Examen Unidad 1: Gramática" class="input-diseño">
+                        </div>
+                        <div class="fila-input">
+                            <label>Tiempo (minutos)</label>
+                            <input type="number" name="tiempo_limite" value="30" class="input-diseño">
+                        </div>
+                        <div class="fila-input">
+                            <label>Fecha Límite</label>
+                            <input type="date" name="fecha_limite" class="input-diseño">
+                        </div>
+                    </div>
+
+                    <div class="fila-input">
+                        <label>Asignar a los alumnos:</label>
+                        <div class="selector-multi-alumnos">
+                            <label class="opcion-alumno"><input type="checkbox" name="alumnos_test[]" value="1"> Carlos Sánchez</label>
+                            <label class="opcion-alumno"><input type="checkbox" name="alumnos_test[]" value="2"> David Sola</label>
+                            <label class="opcion-alumno"><input type="checkbox" name="alumnos_test[]" value="3"> Ana Belén</label>
+                        </div>
+                    </div>
+
+                    <div class="bloque-pregunta-creador">
+                        <div class="fila-input">
+                            <label>Enunciado de la Pregunta</label>
+                            <input type="text" name="pregunta_1" placeholder="Escribe aquí la pregunta..." class="input-diseño">
+                        </div>
+                        <div class="grupo-opciones">
+                            <input type="text" name="p1_opcion_a" placeholder="Opción A (Correcta)" class="input-diseño border-verde">
+                            <input type="text" name="p1_opcion_b" placeholder="Opción B" class="input-diseño">
+                            <input type="text" name="p1_opcion_c" placeholder="Opción C" class="input-diseño">
+                        </div>
+                    </div>
+
+                    <div class="botones-examen">
+                        <button type="button" class="boton-secundario"> + Añadir otra pregunta</button>
+                        <button type="submit" class="boton-accion-naranja">PUBLICAR EXAMEN</button>
+                    </div>
+                </form>
+            </section>
+
+            <section class="caja-herramienta borde-azul ancho-completo">
+                <form action="#" method="POST">
+                    <h3>Foro: Responder Consultas</h3>
+                    <div class="tabla-scroll">
+                        <table class="tabla-gestion">
+                            <thead>
+                                <tr>
+                                    <th>Alumno y Pregunta</th>
+                                    <th>Tu respuesta</th>
+                                    <th>Acción</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <strong>Paco Díaz</strong>
+                                        <p>¿Hay clase el lunes?</p>
+                                    </td>
+                                    <td>
+                                        <input type="text" placeholder="Escribe tu respuesta..." class="input-diseño">
+                                    </td>
+                                    <td>
+                                        <button type="submit" class="boton-responder">Enviar</button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </form>
+            </section>
+
+        </div>
+        
+    </main>
+
+    <footer class="pie-pagina">
+        <div class="contenedor">
+            <p>Academia Enaclass - Valsequillo, Gran Canaria</p>
+            <div class="rayita-naranja"></div>
+            <p class="copyright">© 2026 Todos los derechos reservados</p>
+        </div>
+    </footer>
+
+</body>
+</html>
